7a116599d8422b54e7b24bc6e84120bcb8b547f8
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-05-20 20:52:32 +0900

    コンポーネント作成準備

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css'
+import { Main } from './components/Main'
+import Sidebar from './components/Sidebar'
 
 function App() {
 
   return <div className="App">
-    <h1>Hello</h1>
+    <Sidebar></Sidebar>
+    <Main></Main>
   </div>
 }
 
