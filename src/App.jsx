f6d5c01b44cf48efe9948fc9090331ff4fdfba4b
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-05-26 23:04:05 +0900

    ノート情報の追加（ハードコーディング）

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+import { useState } from 'react'
 import './App.css'
 import { Main } from './components/Main'
 import Sidebar from './components/Sidebar'
 
 function App() {
 
+  //配列を用意してこの配列のノートを追加していく
+  const [notes, setNotes] = useState([]);
+
   const onAddNote = () => {
     console.log("新しくノートが追加されました");
+    const newNote= {
+      id: 1,
+      title: "新しいノート",
+      content: "新しいノートの内容",
+      modDate: Date.now()
+    };
+    setNotes(newNote);
+    console.log(notes);
   }
 
   return <div className="App">
