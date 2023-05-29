40e5d280ca182b46d6b46dbfb28dd670b14e7e60
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-05-30 00:45:10 +0900

    mainノートのHTMLを作成

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,6 +4,15 @@ import "./Main.css";
 
 export const Main = () => {
   return (
-    <div>Main</div>
+    <div className='app-main'>
+      <div className='app-main-note-edit'>
+        <input type="text"/>
+        <textarea id="" placeholder="ノート内容を記入"/>
+        </div>
+        <div className="app-main-note-preview">
+          <h1 className="preview-title">タイトル</h1>
+          <div className="markdown-preview">ノート内容</div>
+        </div>
+      </div>
   )
 }
